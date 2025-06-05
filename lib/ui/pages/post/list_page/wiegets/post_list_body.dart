64f0f86702984397a866c9b6d06f2f4ad24b774e
getinthere <getinthere@naver.com>
2025-06-05 17:02:00 +0900

    로그인

--- lib/ui/pages/post/list_page/wiegets/post_list_body.dart
+++ lib/ui/pages/post/list_page/wiegets/post_list_body.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/gvm/session_gvm.dart';
 import 'package:flutter_blog/ui/pages/post/detail_page/post_detail_page.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_item.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class PostListBody extends StatelessWidget {
+class PostListBody extends ConsumerWidget {
   const PostListBody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    SessionModel model = ref.read(sessionProvider);
+
+    Logger().d("${model}");
+
     return ListView.separated(
       itemCount: 5,
       itemBuilder: (context, index) {
         return InkWell(
           onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (_) => PostDetailPage()));
+            Navigator.push(context, MaterialPageRoute(builder: (_) => PostDetailPage()));
           },
           child: PostListItem(),
         );
