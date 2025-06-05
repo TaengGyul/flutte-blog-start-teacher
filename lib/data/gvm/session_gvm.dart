b70012b0784681b426bbbc8fe6a278843690a613
getinthere <getinthere@naver.com>
2025-06-05 15:42:25 +0900

    회원가입 완료

---
+++ lib/data/gvm/session_gvm.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart' show ScaffoldMessenger, Text, SnackBar, Navigator;
+import 'package:flutter_blog/data/repository/user_repository.dart';
+import 'package:flutter_blog/main.dart';
+import 'package:flutter_blog/ui/pages/auth/join_page/join_fm.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+/// 1. 창고 관리자
+final sessionProvider = NotifierProvider<SessionGVM, SessionModel>(() {
+  // 의존하는 VM
+
+  return SessionGVM();
+});
+
+/// 2. 창고 (상태가 변경되어도, 화면 갱신 안함 - watch 하지마)
+class SessionGVM extends Notifier<SessionModel> {
+  final mContext = navigatorKey.currentContext!;
+
+  @override
+  SessionModel build() {
+    return SessionModel();
+  }
+
+  Future<void> join(String username, String email, String password) async {
+    Logger().d("username : ${username}, email : ${email}, password : ${password}");
+    bool isValid = ref.read(joinProvider.notifier).validate();
+    if (!isValid) {
+      ScaffoldMessenger.of(mContext).showSnackBar(
+        SnackBar(content: Text("유효성 검사 실패입니다")),
+      );
+      return;
+    }
+
+    Map<String, dynamic> body = await UserRepository().join(username, email, password);
+    if (!body["success"]) {
+      ScaffoldMessenger.of(mContext).showSnackBar(
+        SnackBar(content: Text("${body["errorMessage"]}")),
+      );
+      return;
+    }
+
+    Navigator.pushNamed(mContext, "/login");
+  }
+
+  Future<void> login(String username, String password) async {}
+
+  Future<void> logout() async {}
+}
+
+/// 3. 창고 데이터 타입
+class SessionModel {
+  int? id;
+  String? username;
+  String? accessToken;
+  bool? isLogin;
+
+  SessionModel({this.id, this.username, this.accessToken, this.isLogin = false});
+}
