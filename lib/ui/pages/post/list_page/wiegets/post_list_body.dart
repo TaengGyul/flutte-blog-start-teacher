16c009f3c2c9bc6769d5132da6a0c45ee66f5b19
getinthere <getinthere@naver.com>
2025-06-09 11:54:26 +0900

    게시글 목록보기 페이징직전까지 완료

--- lib/ui/pages/post/list_page/wiegets/post_list_body.dart
+++ lib/ui/pages/post/list_page/wiegets/post_list_body.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/data/gvm/session_gvm.dart';
 import 'package:flutter_blog/ui/pages/post/detail_page/post_detail_page.dart';
+import 'package:flutter_blog/ui/pages/post/list_page/post_list_vm.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 
 class PostListBody extends ConsumerWidget {
-  const PostListBody({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    SessionModel model = ref.read(sessionProvider);
+    PostListModel? model = ref.watch(postListProvider);
 
-    Logger().d("${model}");
-
-    return ListView.separated(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => PostDetailPage()));
-          },
-          child: PostListItem(),
-        );
-      },
-      separatorBuilder: (context, index) {
-        return const Divider();
-      },
-    );
+    if (model == null) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return ListView.separated(
+        itemCount: model.posts.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => PostDetailPage()));
+            },
+            child: PostListItem(model.posts[index]),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      );
+    }
   }
 }
