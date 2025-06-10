a13ccf0f8c4ed2b1eb41023c3ec3ae9478b2c08c
TaengGyul <mjcho0130@gmail.com>
2025-06-10 11:13:52 +0900

    게시글 쓰기 ì수정 완료

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -4,11 +4,10 @@ import 'package:logger/logger.dart';
 
 class PostRepository {
   Future<Map<String, dynamic>> write(String title, String content) async {
-    Response response = await dio.post("/api/post", data: {
-      "title": title,
-      "content": content,
-    });
-    return response.data;
+    Response response = await dio.post("/api/post", data: {"title": title, "content": content});
+    final responseBody = response.data;
+    Logger().d(responseBody);
+    return responseBody;
   }
 
   Future<Map<String, dynamic>> getList({int page = 0}) async {
